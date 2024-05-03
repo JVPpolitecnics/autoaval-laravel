4e1c09911c48152772561f67893ae3df1f5469b7
Ainhoa <157162949+JVPpolitecnics@users.noreply.github.com>
2024-05-03 20:14:29 +0200

    works

---
+++ resources/components/criterisView.vue
@@ -0,0 +1,24 @@
+<template>
+
+</template>
+<script>
+ import axios from 'axios'; 
+export default {
+
+    
+    created(){
+
+    },
+
+  components: {
+  
+  },
+  methods: {
+
+  }
+
+}
+</script>
+<style>
+
+</style>
